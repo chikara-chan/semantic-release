88071bebb0b2f29d97f74b53cccba4117ed49b84
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2018-03-12 23:12:32 -0400

    feat: WIP support multiple branches and channels

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const verify = require('./lib/verify');
 const getNextVersion = require('./lib/get-next-version');
 const getCommits = require('./lib/get-commits');
 const getLastRelease = require('./lib/get-last-release');
+const getTags = require('./lib/get-tags');
 const {extractErrors} = require('./lib/utils');
+const getBranches = require('./lib/branches');
 const getGitAuthUrl = require('./lib/get-git-auth-url');
 const logger = require('./lib/logger');
 const {unshallow, verifyAuth, isBranchUpToDate, gitHead: getGitHead, tag, push} = require('./lib/git');
@@ -20,7 +22,7 @@ const {COMMIT_NAME, COMMIT_EMAIL} = require('./lib/definitions/constants');
 marked.setOptions({renderer: new TerminalRenderer()});
 
 async function run(options, plugins) {
-  const {isCi, branch, isPr} = envCi();
+  const {isCi, branch: ciBranch, isPr} = envCi();
 
   if (!isCi && !options.dryRun && !options.noCi) {
     logger.log('This run was not triggered in a known CI environment, running in dry-run mode.');
@@ -43,16 +45,25 @@ async function run(options, plugins) {
     return;
   }
 
-  if (branch !== options.branch) {
+  // Verify config
+  await verify(options);
+
+  // Unshallow the repo in order to get all the tags
+  await unshallow(options.repositoryUrl);
+
+  // Normalize and verify branches
+  options.branches = await getBranches(await getTags(options));
+
+  const branch = options.branches.find(({name}) => name === ciBranch);
+
+  if (branch) {
     logger.log(
-      `This test run was triggered on the branch ${branch}, while semantic-release is configured to only publish from ${
-        options.branch
-      }, therefore a new version won’t be published.`
+      `This test run was triggered on the branch ${ciBranch}, while semantic-release is configured to only publish from ${options.branches
+        .map(({name}) => name)
+        .join(', ')}, therefore a new version won’t be published.`
     );
     return false;
   }
-
-  await verify(options);
 
   options.repositoryUrl = await getGitAuthUrl(options);
 
@@ -71,16 +82,57 @@ async function run(options, plugins) {
     throw getError('EGITNOPERMISSION', {options});
   }
 
-  logger.log('Run automated release from branch %s', options.branch);
-
+  logger.log('Run automated release from branch %s', ciBranch);
   logger.log('Call plugin %s', 'verify-conditions');
   await plugins.verifyConditions({options, logger}, {settleAll: true});
 
-  // Unshallow the repo in order to get all the tags
-  await unshallow(options.repositoryUrl);
+  const lastRelease = await getLastRelease(branch, logger);
 
-  const lastRelease = await getLastRelease(options.tagFormat, logger);
-  const commits = await getCommits(lastRelease.gitHead, options.branch, logger);
+  // TODO
+  // If lastRelease is also present in any upper branch which is not a prerelease
+  //  Check if in range of its branch
+  //    => if not throw error
+  //    => If yes, nextRelease is lastRelease and call publish addChannel and success/fail
+  // If not a merge, get next version and check if is in range
+  //  => if not throw error
+  //  => If yes, compute nextRelease with getCommits, analyzeCommits and call verifyRelease and prepare
+
+  // Send commits when changing channel? Probably not. Make more sens to have dedicated plugin for channel change
+
+  // if (isMerge(lastRelease, branches)) {
+  //   if (semver.satisfies(lastRelease.version, branch.range)) {
+  //     nextRelease = lastRelease;
+  //     // Call addChannel and success/fail
+  //     // Or return lastRelease and nextRelease
+  //   } else {
+  //     // throw error illegal merge
+  //   }
+  // } else {
+  //   const commits = await getCommits(lastRelease.gitHead, branch, logger);
+  //   const type = await plugins.analyzeCommits({
+  //     options,
+  //     logger,
+  //     lastRelease,
+  //     commits: commits.filter(commit => !/\[skip\s+release\]|\[release\s+skip\]/i.test(commit.message)),
+  //   });
+  //   if (!type) {
+  //     logger.log('There are no relevant changes, so no new version is released.');
+  //     return;
+  //   }
+  //   const version = getNextVersion(type, lastRelease, logger);
+  //
+  //   const nextRelease = {
+  //     type,
+  //     version,
+  //     channel,
+  //     gitHead: await getGitHead(),
+  //     gitTag: template(options.tagFormat)({version}),
+  //   };
+  //   // Or return lastRelease and null?
+  // }
+
+  const {channel} = branch;
+  const commits = await getCommits(lastRelease.gitHead, branch, logger);
 
   logger.log('Call plugin %s', 'analyze-commits');
   const type = await plugins.analyzeCommits({
@@ -93,8 +145,20 @@ async function run(options, plugins) {
     logger.log('There are no relevant changes, so no new version is released.');
     return;
   }
+
+  // TODO rework next version for determining the version of pre-release
+  // TODO determine next version only if not a merge
   const version = getNextVersion(type, lastRelease, logger);
-  const nextRelease = {type, version, gitHead: await getGitHead(), gitTag: template(options.tagFormat)({version})};
+  // TODO verify if release match branch range (even it's a downstream merge)
+  // TODO pass channel
+  // TODO in case of merge pass the lastRelease as nextRelease
+  const nextRelease = {
+    type,
+    version,
+    channel,
+    gitHead: await getGitHead(),
+    gitTag: template(options.tagFormat)({version}),
+  };
 
   logger.log('Call plugin %s', 'verify-release');
   await plugins.verifyRelease({options, logger, lastRelease, commits, nextRelease}, {settleAll: true});
@@ -141,12 +205,12 @@ async function run(options, plugins) {
       {transform: (release, step) => ({...(isPlainObject(release) ? release : {}), ...nextRelease, ...step})}
     );
 
+    logger.log('Published release: %s', nextRelease.version);
+
     await plugins.success(
       {options, logger, lastRelease, commits, nextRelease, releases: castArray(releases)},
       {settleAll: true}
     );
-
-    logger.log('Published release: %s', nextRelease.version);
   }
   return true;
 }
